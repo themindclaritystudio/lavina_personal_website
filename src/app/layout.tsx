a1a512d8c9f1ee7e3e0ad003d018558e5ad57c8b
ARUN <arunjdev26@gmail.com>
2026-06-17 14:13:03 +0530

    update on pack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,38 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Layout/Header";
-import Footer from "@/components/Layout/Footer";
-import { ThemeProvider } from "next-themes";
-import ScrollToTop from '@/components/ScrollToTop';
-import Aoscompo from "@/utils/aos";
-import NextTopLoader from 'nextjs-toploader';
-import SessionProviderComp from "@/components/nextauth/SessionProvider";
-import { AuthDialogProvider } from "./context/AuthDialogContext";
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/Layout/Header'
+import Footer from '@/components/Layout/Footer'
+import { ThemeProvider } from 'next-themes'
+import ScrollToTop from '@/components/ScrollToTop'
+import Aoscompo from '@/utils/aos'
+import { AuthDialogProvider } from './context/AuthDialogContext'
 
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
-  session,
-}: Readonly<{
-  children: React.ReactNode;
-  session:any
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-      
-      <AuthDialogProvider>
-      <SessionProviderComp session={session}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={false}
-          defaultTheme="light"
-        >
-          <Aoscompo>
-            <Header />
-            {children}
-            <Footer />
-          </Aoscompo>
-          <ScrollToTop />
-        </ThemeProvider>
-        </SessionProviderComp>
+        <AuthDialogProvider>
+            <ThemeProvider
+              attribute="class"
+              enableSystem={false}
+              defaultTheme="light"
+            >
+              <Aoscompo>
+                <Header />
+                {children}
+                <Footer />
+              </Aoscompo>
+
+              <ScrollToTop />
+            </ThemeProvider>
         </AuthDialogProvider>
       </body>
     </html>
-  );
+  )
 }
