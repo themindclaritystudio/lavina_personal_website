98d77eff2817e467edaf9eb5a73c7b4f9b5fd885
ARUN <arunjdev26@gmail.com>
2026-06-17 13:51:23 +0530

    Update website content and design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
      
       {/* <Testimonial />
       <Blog /> */}
-      {/* <Experience /> */}
+       {/* <Experience /> */}
       {/* <Contactform /> */}
     </main>
   )
