db9cb27dc65d82208172a011e0aece510e9a8433
ARUN <arunjdev26@gmail.com>
2026-07-14 16:02:27 +0530

    fav change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,17 +68,8 @@ export const metadata: Metadata = {
       {
         url: "/favicon.ico",
         sizes: "any",
-      },
-      {
-        url: "/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
+      }
+     
     ],
     apple: [
       {
